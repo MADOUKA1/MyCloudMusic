89d3a5c66d3be8cb2a3d58e202ed8bef86d61052
yw <yw@163.com>
2020-09-11 22:19:05 +0800

    3518 add splash activity full screen.

--- MyCloudMusic/app/src/main/java/yangwu/bilibili/mycloudmusic/activity/SplashActivity.java
+++ MyCloudMusic/app/src/main/java/yangwu/bilibili/mycloudmusic/activity/SplashActivity.java
@@ -2,7 +2,9 @@ package yangwu.bilibili.mycloudmusic.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 
 import yangwu.bilibili.mycloudmusic.R;
 
@@ -12,5 +14,22 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //设置界面全屏
+        View decorView = getWindow().getDecorView();
+        //判断版本
+        if(Build.VERSION.SDK_INT>=19){
+            //SYSTEM_UI_FLAG_HIDE_NAVIGATION:隐藏导航栏
+            //SYSTEM_UI_FLAG_IMMERSIVE_STICKY:从状态栏下拉会半透明悬浮显示一会儿状态栏和导航栏
+            //SYSTEM_UI_FLAG_FULLSCREEN:全屏
+            int options = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION |
+                    View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY |
+                    View.SYSTEM_UI_FLAG_FULLSCREEN;
+
+            //设置到控件
+            decorView.setSystemUiVisibility(options);
+        }
+
+
     }
 }
